2664180f4b35d93d2ff6d736c5b805da8850d046
manami-shinoda <manami.snd.125@gmail.com>
2021-07-25 22:59:49 +0900

    [カウンタ]を学習した

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -4,6 +4,11 @@ const GenkiFunction = function(number) {
   for (let n = 1; n <= number; n++) {
     if (n % 3 === 0) {
       console.log(n + "!!!!!!")
+    } else if (n % 10 === 3) {
+      console.log(n + "!!!!!!")
+    } else if (String(n).match(/3/)) {
+      //nをstring型に変換し、3が含まれているか調べる！
+      console.log(n + "!!!!!!")
     } else {
       console.log(n)
     }
